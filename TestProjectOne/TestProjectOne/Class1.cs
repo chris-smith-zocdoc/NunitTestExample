888c50253106dac91869cba98f84add590f45d0e
chris.smith <chris.smith@zocdoc.com>
2016-10-18 13:33:47 -0400

    initial commit

---
+++ TestProjectOne/TestProjectOne/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using NUnit.Framework;
+
+namespace TestProjectOne
+{
+    public class Class1
+    {
+        [TestCase(10)]
+        [TestCase(100)]
+        [TestCase(1000)]
+        public void MyTestCase(int num)
+        {
+            Thread.Sleep(TimeSpan.FromMilliseconds(num));
+        }
+    }
+
+    public class Class2
+    {
+        [TestCase(10)]
+        [TestCase(100)]
+        [TestCase(1000)]
+        public void MyTestCase(int num)
+        {
+            Thread.Sleep(TimeSpan.FromMilliseconds(num));
+        }
+    }
+}
---
+++ TestProjectOne/TestProjectTwo/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using NUnit.Framework;
+
+namespace TestProjectTwo
+{
+    public class Class1
+    {
+        [TestCase(20)]
+        [TestCase(200)]
+        [TestCase(2000)]
+        public void AnotherTestCase(int num)
+        {
+            Thread.Sleep(TimeSpan.FromMilliseconds(num));
+        }
+    }
+
+    public class Class2
+    {
+        [TestCase(20)]
+        [TestCase(200)]
+        [TestCase(2000)]
+        public void AnotherTestCase(int num)
+        {
+            Thread.Sleep(TimeSpan.FromMilliseconds(num));
+        }
+    }
+}
